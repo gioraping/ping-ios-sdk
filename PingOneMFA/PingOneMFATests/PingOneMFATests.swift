e609241c56312de4568d346a102506d20b2df9a2
Giora Krasilshchik <giorakrasilshchik@pingidentity.com>
2026-05-20 16:12:59 +0300

    project file

---
+++ PingOneMFA/PingOneMFATests/PingOneMFATests.swift
@@ -0,0 +1,19 @@
+//
+//  PingOneMFATests.swift
+//  PingOneMFATests
+//
+//  Created by Giora Krasilshchik on 20/05/2026.
+//
+
+import Testing
+@testable import PingOneMFA
+
+struct PingOneMFATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
